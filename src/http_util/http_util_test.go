5da7b8e8e04574497064d86d053b14d8daf8165f
Fengyuan Chen <cfy1990@gmail.com>
2015-05-25 14:25:56 +0800

    minor changes

--- src/http_util/http_util_test.go
+++ src/http_util/http_util_test.go
@@ -6,16 +6,16 @@ import (
 )
 
 func TestResourceStat(t *testing.T) {
-	stat, err := GetResourceStat("http://7xislp.com1.z0.glb.clouddn.com/a.mp3", http.Header{})
+	stat, err := GetResourceStat("http://7xislp.com1.z0.glb.clouddn.com/b.mp3", http.Header{})
 	if err != nil {
 		t.Errorf("%v", err)
 	} else {
 		t.Logf("%v", stat)
 	}
-	if stat.length != 214483 {
+	if stat.length != 42 {
 		t.Errorf("wrong content length, except 42, get %v", stat.length)
 	}
-	if stat.filename != "a.mp3" {
-		t.Errorf("wrong filename, except a.mp3, get %v", stat.filename)
+	if stat.filename != "b.mp3" {
+		t.Errorf("wrong filename, except b.mp3, get %v", stat.filename)
 	}
 }
