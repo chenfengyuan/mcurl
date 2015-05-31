70dc505f3a8577b8b3a5ac4c80e3880105c72457
Dongmei Ban <bancage@gmail.com>
2015-05-31 21:36:21 +0800

    add host in log

--- src/http_util/http_util.go
+++ src/http_util/http_util.go
@@ -117,7 +117,12 @@ func Downloader(task_info_c <-chan DownloadTaskInfo, finished_c chan<- DownloadC
 		length := task_info.Length
 		start := task_info.Start
 		name := task_info.Name
-		log.Printf("Worker[%v] %v %v", name, worker_n, task_info.DownloadRange)
+		url_obj, err := url.Parse(task_info.Request.url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		host := url_obj.Host
+		log.Printf("Worker[%v] %v %v %v", name, host, worker_n, task_info.DownloadRange)
 		var downloaded int64 = 0
 		task_start_time := GetNowEpochInMilli()
 		for try_times := 0; try_times < 3; try_times++ {
@@ -125,7 +130,7 @@ func Downloader(task_info_c <-chan DownloadTaskInfo, finished_c chan<- DownloadC
 			go RangeGet(task_info.Request, start, length-(start-task_info.Start), chunk_datas)
 			for chunk_data := range chunk_datas {
 				downloaded += int64(len(chunk_data))
-				log.Printf("Wroker[%v] %v %v k/s %v%%", name, worker_n, downloaded*1000/1024/(GetNowEpochInMilli()-task_start_time), 100*downloaded/length)
+				log.Printf("Wroker[%v] %v %v %v k/s %v%%", name, host, worker_n, downloaded*1000/1024/(GetNowEpochInMilli()-task_start_time), 100*downloaded/length)
 				finished_c <- DownloadChunk{Data: chunk_data, Name: name, Start: start}
 				start += int64(len(chunk_data))
 			}
